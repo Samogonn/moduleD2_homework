17c49e5d013208cb36931b12cda772947b0cf9d5
Yuri Solovev <solovevym@gmail.com>
2023-04-19 10:17:41 +0300

    group permissions

--- accounts/models.py
+++ accounts/models.py
@@ -22,67 +22,6 @@ class Author(models.Model):
         self.save()
 
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=64, unique = True)
-
-
-class Post(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    NEWS = 'NW'
-    ARTICLE = 'AR'
-    POST_TYPE_CHOICES = [
-        (NEWS, 'Новость'),
-        (ARTICLE, 'Статья')
-    ]
-
-    type = models.CharField(
-        max_length=2,
-        choices=POST_TYPE_CHOICES,
-        default=NEWS
-    )
-    datetime = models.DateTimeField(auto_now_add=True)
-    category = models.ManyToManyField(Category, through='PostCategory')
-    title = models.CharField(max_length=128)
-    text = models.TextField()
-    rating = models.SmallIntegerField(default=0)
-
-    def like(self):
-        self.rating += 1
-        self.save()
-
-    def dislike(self):
-        self.rating -= 1
-        self.save()
-
-    def preview(self):
-        return self.text[:124] + '…'
-
-    def get_absolute_url(self):
-        return f'/news/{self.id}'
-
-
-class PostCategory(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=512)
-    datetime = models.DateTimeField(auto_now_add=True)
-    rating = models.SmallIntegerField(default=0)
-
-    def like(self):
-        self.rating += 1
-        self.save()
-
-    def dislike(self):
-        self.rating -= 1
-        self.save()
-
-
 class BasicSignupForm(SignupForm):
     def save(self, request):
         user = super(BasicSignupForm, self).save(request)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news_list'
+    queryset = Post.objects.order_by('-id')
+    paginate_by = 3
+
+
+class CreatePost(PermissionRequiredMixin, CreateView):
+    template_name = 'create_post.html'
+    form_class = PostForm
+    permission_required = ('news.add_post', )
+
+
+class Search(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'news_details.html'
+    context_object_name = 'news_details'
+
+
+
+class UpdatePost(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    template_name = 'create_post.html'
+    form_class = PostForm
+    permission_required = ('news.change_post', )
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+class DeletePost(DeleteView):
+    template_name = 'post_delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news/'
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/protect/')
